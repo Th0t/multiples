1e1e995f738988cce6d3b7683997f46abcc724be
th0t <julien.vanderstock@protonmail.ch>
2020-11-12 12:54:16 +0100

    One liner

--- main.rb
+++ main.rb
@@ -2,11 +2,10 @@
 
 puts "Please enter a value for n (Interger > 0)"
 n = gets.chomp.to_i
+raise 'n should be an integer' unless n.is_a?(Integer)
+raise 'think positive' if n < 0
 
 def get_multiples_sum(n)
-  raise 'n should be an integer' unless n.is_a?(Integer)
-  raise 'think positive' if n < 0
-
   return 0 if n == 0
 
   multiples = (0...n).select { |i| i%3 == 0 || i%5 == 0 || i%7 == 0 }
@@ -17,4 +16,9 @@ def get_multiples_sum(n)
   multiples.reduce(:+)
 end
 
+def get_multiples_sum_2(n)
+  (0...n).inject(0) { |sum, i| i%3 == 0 || i%5 == 0 || i%7 == 0 ? sum += i : sum }
+end
+
 puts "Sum of that collection is : #{get_multiples_sum(n)}"
+puts "Sum of that collection is : #{get_multiples_sum_2(n)}"
